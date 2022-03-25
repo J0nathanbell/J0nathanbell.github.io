0754b939cef174b32b005e9d5b94598880afadf1
Jonathan Bell <jonnybell123@hotmail.com>
2022-03-25 17:02:26 +0000

    removed contac t us page and added material ui links to the topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,21 +1,35 @@
 import "./topbar.scss"
 import EmailIcon from '@mui/icons-material/Email';
-import PersonIcon from '@mui/icons-material/Person';
-
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 export default function topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={'topbar ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">.Jonathan</a>
+          <a href="#intro" className="logo">.Jonathan.</a>
           <div className="itemContainer">
-            <PersonIcon className="icon"/>
-            <span>+44 (0)772 244 2278</span>
+            <a href="tel:+447722442278" aria-label="0 7 7. 2 2 4. 4 2 2 7 8."><span><PhoneIphoneIcon className="icon"/></span></a>
           </div>
           <div className="itemContainer">
-            <EmailIcon className="icon"/>
-            <span>jonathanbell222@protonmail.com</span>
+            <span><a href="mailto:jonnybell123@hotmail.com?subject=Enquiry"><EmailIcon className='icon'/></a></span>
           </div>
+          <div className="itemContainer">
+            <span><a href="https://github.com/J0nathanbell"></a><GitHubIcon className='icon'/></span>
+          </div>
+          <div className="itemContainer">
+            <span><a href="https://www.facebook.com/profile.php?id=100073074377173"><FacebookIcon className='icon'/></a></span>
+          </div>
+          <div className="itemContainer">
+            <span><a href="https://www.linkedin.com/in/jonathanbell123/"><LinkedInIcon className='icon'/></a></span>
+          </div>
+
+
+
+
+
         </div>
         <div className="right">
           <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
