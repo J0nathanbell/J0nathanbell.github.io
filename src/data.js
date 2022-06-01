0a772e81ee67a27174ccc81574049f926f6245c1
Jonathan Bell <jonnybell123@hotmail.com>
2022-06-01 09:02:35 +0100

    changed a few bits

--- src/data.js
+++ src/data.js
@@ -1,12 +1,10 @@
 export const featuredPortfolio = [
   {
-    id: 1,
     title: "App",
     img: require('./assets/projects/apps/whatever/app.gif'),
     appLink: "https://whateverjb.herokuapp.com/"
   },
   {
-    id: 1,
     github: require('./assets/icons/github.png'),
     link: "https://github.com/J0nathanbell/whatever",
     heroku: require('./assets/icons/Heroku.png'),
@@ -16,7 +14,6 @@ export const featuredPortfolio = [
     icon3: require('./assets/icons/js.png'),
   },
   {
-    id: 1,
     title: "Team",
     img: require('./assets/projects/apps/whatever/team.png'),
     appLink: "https://www.youtube.com/watch?v=jR9xcC7LZD4"
@@ -25,16 +22,13 @@ export const featuredPortfolio = [
     paragraph: 'A movie and food pairing app built with ruby on rails'
   }
 ];
-
 export const webappPortfolio = [
   {
-    id: 2,
     title: "App",
     img: require('./assets/projects/apps/scareBnB/scareBnBDemoVideo.webp'),
     appLink: "https://airbnb-scoomey.herokuapp.com/"
   },
   {
-    id: 2,
     github: require('./assets/icons/github.png'),
     link: "https://github.com/Scoomey/whatever",
     heroku: require('./assets/icons/Heroku.png'),
@@ -44,52 +38,43 @@ export const webappPortfolio = [
     icon3: require('./assets/icons/js.png'),
   },
   {
-    id: 2,
     title: "Team",
     img: require('./assets/projects/apps/whatever/team.png')
   },
   {
     paragraph: "A airBnB clone, selling horror movie villan, 'rent-a-scairs'"
   }
-
 ];
-
-
-
-
 export const mobilePortfolio = [
   {
-    id: 3,
     title: "App",
     img: require('./assets/projects/apps/pokedex/pokemon.gif'),
     appLink: "https://j0nathanbell.github.io/pokemon/"
   },
   {
-    id: 3,
     github: require('./assets/icons/github.png'),
     link: "https://github.com/J0nathanbell/pokemon",
     ghpages: require('./assets/icons/ghpages.png'),
     link2: 'https://j0nathanbell.github.io/pokemon/',
     icon3: require('./assets/icons/js.png'),
   },
+  {},
   {
     paragraph: 'A pokedex made with vanilla JS',
   }
 ];
-
 export const designingPortfolio = [
   {
-    id: 3,
     title: "App",
     img: require('./assets/projects/apps/rejectionClose/python.gif'),
     appLink: "https://github.com/J0nathanbell/large_screen_rejection_close"
   },
   {
-    id: 3,
     github: require('./assets/icons/github.png'),
     link: "https://github.com/J0nathanbell/large_screen_rejection_close",
     icon3: require('./assets/icons/py.png'),
   },
+  {},
   {
     paragraph: 'My first app, a mouse automation app for filling data into forms',
   }
