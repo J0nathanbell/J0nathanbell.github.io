a3a3c20c1f60b0ed9172a4975524c8a390050ea7
J0nathanbell <jonnybell123@hotmail.com>
2023-06-07 12:27:29 +0100

    fixed the cross animation on intro page and the icons turning red when topbar class active

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,39 +1,56 @@
-import './intro.scss'
-import { init } from 'ityped'
-import { useEffect, useRef } from "react"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-export default function Intro({menuOpen}) {
+export default function Intro({ menuOpen }) {
   const textRef = useRef();
 
-  useEffect(()=>{
+  useEffect(() => {
     init(textRef.current, {
       showCursor: true,
       backDelay: 1000,
-      strings: ['A dab-hand with HTML', 'A progigy with Python', '"The" NodeJS Ninja', 'The CSS Swashbuckler', 'The wrangler of Ruby', 'The terminal terminator', 'The Giga Chad of git', 'Supreame Ruler of React', 'The Reaper of Rails', 'Like Jesus with Javascript']
-    })
-  },[]);
+      strings: [
+        "A dab-hand with HTML",
+        "A progigy with Python",
+        '"The" NodeJS Ninja',
+        "The CSS Swashbuckler",
+        "The wrangler of Ruby",
+        "The terminal terminator",
+        "The Giga Chad of git",
+        "Supreame Ruler of React",
+        "The Reaper of Rails",
+        "Like Jesus with Javascript",
+      ],
+    });
+  }, []);
 
   return (
     <div className="intro" id="intro">
-      <div className='left'>
-        <div className={"imgContainer " + (menuOpen && 'active')}>
-          <img className={"top " + (menuOpen && "active")} src={require('../../assets/myPicture/coolguy3.png')} alt="" />
+      <div className="left">
+        <div className={"imgContainer " + (menuOpen && "active")}>
+          <img
+            className={"top " + (menuOpen && "active")}
+            src={require("../../assets/myPicture/coolguy3.png")}
+            alt=""
+          />
         </div>
       </div>
-      <div className='right'>
+      <div className="right">
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Jonathan Bell</h1>
           <h3>Some folks describe me as...</h3>
-          <h3><span ref={textRef}></span></h3>
-          <a href="#portfolio">
-            <div className="arrowContainer">
-              <span className={"arrow1 "+ (menuOpen && 'active')}></span>
-              <span className={"arrow2 "+ (menuOpen && 'active')}></span>
+          <h3>
+            <span ref={textRef}></span>
+          </h3>
+          <a className="arrow-container-link" href="#portfolio">
+            <div className="arrow-container">
+              <span className={"arrow-1 " + (menuOpen && "active")}></span>
+              <span className={"arrow-2 " + (menuOpen && "active")}></span>
             </div>
           </a>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,25 +1,49 @@
-import "./topbar.scss"
-import EmailIcon from '@mui/icons-material/Email';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-export default function topbar({menuOpen, setMenuOpen}) {
+import "./topbar.scss";
+
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+export default function topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className={'topbar ' + (menuOpen && 'active')}>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <div className="nameContainer">
-            <span className="logo"><a href="#intro" >.Jonathan.</a></span>
+            <span className="logo">
+              <a id="myname" href="#intro">
+                .Jonathan.
+              </a>
+            </span>
           </div>
           <div className="iconContainer">
-            <span className="icon"><a href="mailto:jonnybell123@hotmail.com?subject=Enquiry"><EmailIcon/></a></span>
-            <span className="icon"><a href="https://github.com/J0nathanbell"><GitHubIcon/></a></span>
-            <span className="icon"><a href="https://www.facebook.com/profile.php?id=100073074377173"><FacebookIcon/></a></span>
-            <span className="icon"><a href="https://www.linkedin.com/in/jonathanbell123/"><LinkedInIcon/></a></span>
+            <span className="icon">
+              <a href="mailto:jonnybell123@hotmail.com?subject=Enquiry">
+                <EmailIcon />
+              </a>
+            </span>
+            <span className="icon">
+              <a href="https://github.com/J0nathanbell">
+                <GitHubIcon />
+              </a>
+            </span>
+            <span className="icon">
+              <a href="https://www.facebook.com/profile.php?id=100073074377173">
+                <FacebookIcon />
+              </a>
+            </span>
+            <span className="icon">
+              <a href="https://www.linkedin.com/in/jonathanbell123/">
+                <LinkedInIcon />
+              </a>
+            </span>
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <div
+            className={"hamburger " + (menuOpen && "active")}
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
@@ -27,5 +51,5 @@ export default function topbar({menuOpen, setMenuOpen}) {
         </div>
       </div>
     </div>
-  )
+  );
 }
