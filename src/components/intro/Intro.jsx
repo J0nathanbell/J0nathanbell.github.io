43b0345bc32614978ef5788f4d0a960d6603872f
J0nathanbell <jonnybell123@hotmail.com>
2025-08-18 15:45:16 +0100

    bosh

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -75,7 +75,7 @@ export default function Intro() {
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi There, I'm</h2>
+          <h2>Hi There, I'm </h2>
           <h1>Jonathan</h1>
           <h3>But some folks have known to call me...</h3>
           <h3>
