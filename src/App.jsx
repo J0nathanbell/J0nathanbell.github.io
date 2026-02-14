5c2b975f5610c2718d4f95a9357fa2ad87bbddc4
jonathan bell <jonnnybell123@hotmail.com>
2026-02-14 14:48:57 +0000

    few loose ends

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 // import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro";
 import Me from "./components/me/Me";
 import Portfolio from "./components/portfolio/Portfolio";
 import "./app.scss";
 import { pokemonPortfolio, ipsoPortfolio } from "./data.js";
-import AOS from 'aos';
-import 'aos/dist/aos.css';         // This brings in the styles (CSS)
+import AOS from "aos";
+import "aos/dist/aos.css"; // This brings in the styles (CSS)
 AOS.init({
-    once: true  // Optional: makes ALL animations happen only once
-  });
+  once: true, // Optional: makes ALL animations happen only once
+});
 function App() {
   return (
     <div className="app">
